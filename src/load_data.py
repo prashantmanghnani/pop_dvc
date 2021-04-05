20c17e5c105b275ba2ded7f3b2158e1abe285dfa
prashantmanghnani <prashantmanghnani.it18@jecrc.ac.in>
2021-04-05 15:48:50 +0530

    tracker added

--- src/load_data.py
+++ src/load_data.py
@@ -15,5 +15,5 @@ def load_and_save(config_path):
 if __name__=="__main__":
     args = argparse.ArgumentParser()
     args.add_argument("--config", default="params.yaml")
-    parsed_args = args.parse_args()
+    parsed_args = args.parse_args() 
     load_and_save(config_path=parsed_args.config) 
